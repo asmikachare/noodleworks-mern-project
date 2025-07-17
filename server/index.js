5a38970b79a14a332edc8a9353b26db89033d366
Asmi Kachare <asmikachare@gmail.com>
2025-07-16 20:06:01 -0700

    Fix CORS origin for frontend

--- server/index.js
+++ server/index.js
@@ -6,7 +6,12 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://noodleworks-frontend.netlify.app',
+  methods: ['GET', 'POST'],
+  credentials: true
+}));
+
 app.use(express.json());
 
 
